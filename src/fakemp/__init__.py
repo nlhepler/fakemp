5eb0cbba5f52415cbac32befdee823d3a2cabd53
Lance Hepler <nlhepler@gmail.com>
2011-11-08 17:38:45 -0800

    Add new interfaces for sanely farming out work using multiprocessing

--- src/fakemp/__init__.py
+++ src/fakemp/__init__.py
@@ -1,5 +1,6 @@
 
 from multiprocessing import Pool, cpu_count, current_process
+from sys import exc_info, exit as sys_exit
 
 try:
     import cPickle as pickle
@@ -7,7 +8,7 @@ except ImportError:
     import pickle
 
 
-__all__ = ['FakeLock', 'FakeResult', 'FakePool', 'create_pool']
+__all__ = ['FakeLock', 'FakeResult', 'FakePool', 'create_pool', 'farmout', 'farmworker']
 
 
 class FakeLock(object):
@@ -69,3 +70,55 @@ def create_pool(test_instance):
         pool = FakePool()
 
     return pool
+
+
+def farmout(num, setup, worker, isresult, attempts=3):
+    try:
+        results = [None] * num
+        undone = xrange(num)
+        for _ in xrange(attempts):
+            pool = create_pool(worker)
+
+            for i in undone:
+                results[i] = pool.apply_async(worker, setup(i))
+
+            pool.close()
+            pool.join()
+
+            for i in undone:
+                results[i] = results[i].get(0xFFFF)
+
+            if any([isinstance(r, KeyboardInterrupt) for r in results]):
+                raise KeyboardInterrupt
+            elif all([isresult(r) for r in results]):
+                break
+            else:
+                undone = [i for i, r in enumerate(results) if not isresult(r)]
+
+        excs = [e for e in results if isinstance(e, Exception)]
+        if len(excs):
+            raise excs[0]
+
+        if not all([isresult(r) for r in results]):
+            raise RuntimeError("Random and unknown weirdness happened while trying to farm out work to child processes")
+
+        return results
+
+    except KeyboardInterrupt, e:
+        if pool is not None:
+            pool.terminate()
+            pool.join()
+        if current_process().daemon:
+            return e
+        else:
+            print 'caught ^C (keyboard interrupt), exiting ...'
+            sys_exit(-1)
+
+
+def farmworker(fn, *args, **kwargs):
+    try:
+        return fn(*args, **kwargs)
+    except KeyboardInterrupt:
+        return KeyboardInterrupt
+    except:
+        return exc_info()[1]
